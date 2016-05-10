76260bdfe33a86a40063be86e8b7592c4dbf192a
Sergey <sergeyv@gmail.com>
2016-05-09 23:01:50 -0700

    changed to 'top posts of all time' and added nsfw filter

--- cogs/redditpics.py
+++ cogs/redditpics.py
@@ -8,7 +8,9 @@ class RedditPics:
 
     def __init__(self, bot):
         self.bot = bot
-        self.loop = asyncio.get_event_loop()
+        self.post = None
+        self.random_post = None
+        self.is_nsfw = False
 
     @commands.command(hidden=True)
     async def sub(self, *message):
@@ -20,30 +22,55 @@ class RedditPics:
             return
         elif message[0].startswith("r/") or message[0].startswith("/r/"):
             subreddit = message[0][message[0].rindex("/")+1:].strip()
-            self.bot.say("Looking up info for {}".format(subreddit))
+            await self.bot.say("Looking up info for {}".format(subreddit))
 
             # do stuff
-            self.get_reddit_top(subreddit)
-            self.get_random_post(self.reddit_reply['data']['children'])
 
-            await self.bot.say(self.random_post['data']['url'])
+            post = self.parse_results(subreddit)
+            print(post)
+            print(post['data']['over_18'])
+            self.is_nsfw = post['data']['over_18']
+            while self.is_nsfw:
+                print("nsfw found")
+                post = self.parse_results(subreddit)
+                self.is_nsfw = post['data']['over_18']
+
+            # reddit_results = self.get_reddit_top(subreddit)
+            # print(len(reddit_results))
+            #
+            # random_post = self.get_random_post(reddit_results)
+            #
+            # post = random_post['data']['url']
+            #
+            # print(post)
+            await self.bot.say(post['data']['url'])
             return
         else:
             await self.bot.say("Invalid syntax.")
             return
 
+    def parse_results(self, subreddit: str):
+        reddit_results = self.get_reddit_top(subreddit)
+
+        print(len(reddit_results))
+
+        post = self.get_random_post(reddit_results)
+
+        #post = random_post['data']['url']
+
+        return post
 
     def get_reddit_top(self, subreddit):
-        url = "https://www.reddit.com/r/{0}/top/.json".format(subreddit)
+        url = "https://www.reddit.com/r/{0}/top/.json?sort=top&t=all".format(subreddit)
         client = requests.session()
         client.get(url)
         header = { 'User-Agent' : 'trying to learn this async thing' }
         reddit_reply = client.get(url, headers=header)
-        self.reddit_reply =  reddit_reply.json()
+        return reddit_reply.json()['data']['children']
 
     # not working yet, returns a random post
     def get_random_post(self,random_post: list):
-        self.random_post = random.choice(random_post)
+        return random.choice(random_post)
 
 
 
