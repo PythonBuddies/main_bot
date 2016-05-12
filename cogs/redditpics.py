2187777f7f5f1568e822f53f031868421969c237
Sergey <sergeyv@gmail.com>
2016-05-11 23:01:35 -0700

    requests->aiohttp, restructured random image picker, restructured nsfw filter

--- cogs/redditpics.py
+++ cogs/redditpics.py
@@ -1,85 +1,76 @@
 __author__ = 'pico'
 
 from discord.ext import commands
-import requests
 import random
+import aiohttp
+import asyncio
+
 
 # reddit pics cog
 class RedditPics:
 
     def __init__(self, bot):
         self.bot = bot
-        self.is_nsfw = False
+        self.session = aiohttp.ClientSession()
 
     @commands.command(hidden=True)
     async def sub(self, *message):
         if len(message) == 0:
             await self.bot.say("Usage: sub r/<subreddit name>")
-            return
         elif len(message) > 1:
             await self.bot.say("Too many parameters.")
-            return
         elif message[0].startswith("r/") or message[0].startswith("/r/"):
             subreddit = message[0][message[0].rindex("/")+1:].strip()
             await self.execute_request(subreddit)
-            return
         else:
             await self.bot.say("Invalid syntax.")
-            return
 
     async def execute_request(self, subreddit):
 
         # download top reddit images for a given subreddit
-        reddit_results = self.get_reddit_top(subreddit)
-
-        # randomly pick a post from the above results
-        post = self.get_random_post(reddit_results)
+        reddit_results = await self.fetch(self.session, subreddit)
+        reddit_results = reddit_results['data']['children']
 
         # if no posts are found, exit
         if len(reddit_results) == 0:
             await self.bot.say("No results found.")
-            return
 
-        # if no images were found in the results, exit
+        # randomly pick a post from the above results
+        post = self.get_random_post(reddit_results)
+
         if post == None:
+            # if no images were found in the results, exit
             await self.bot.say("No image results found.")
-            return
-
-        # check for NSFW content, exit the search if found
-        self.is_nsfw = post['data']['over_18']
-        if self.is_nsfw:
-            await self.bot.say("NSFW content found, exiting.")
-            return
-
-        # this is where the bot replies with what she found
-        await self.bot.say(post['data']['url'])
-
+        else:
+            # this is where the bot replies with what she found
+            await self.bot.say(post)
 
     # connect to reddit and download the top posts of all time for that subreddit (limit 25)
-    def get_reddit_top(self, subreddit):
+    # TODO make this function download multiple pages of 25 posts for better result variety
+    async def fetch(self, session, subreddit):
+        header = {'User-Agent': 'trying to learn this async thing'}
         url = "https://www.reddit.com/r/{0}/top/.json?sort=top&t=all".format(subreddit)
-        client = requests.session()
-        client.get(url)
-        header = { 'User-Agent' : 'trying to learn this async thing' }
-        reddit_reply = client.get(url, headers=header)
 
-        return reddit_reply.json()['data']['children']
+        with aiohttp.Timeout(10):
+            async with session.get(url, headers=header) as response:
+                return await response.json()
 
-    # pick a random post from the results, ensure that post is an image
-    # if not an image, cycle through the posts
+    # pick a random post from the results, ensure that post is an image and is SFW
+    # return None if no images found
     def get_random_post(self, reddit_results: list):
-        # randomize a list of numbers the length of the reddit results download
-        # used to randomly cycle through the results
-        iteration = random.sample(range(0, len(reddit_results)), len(reddit_results))
+        picture_list = []
 
-        while len(iteration) is not 0:
-            result = reddit_results[iteration.pop()]
-            if "imgur" not in result['data']['url']:
-                continue
-            else:
-                return result
-        self.bot.say("Could not retrieve any picture from the results.")
-        return None
+        for item in reddit_results:
+            if "imgur" in item['data']['url']:
+                if item['data']['over_18']:
+                    continue
+                picture_list.append(item['data']['url'])
+
+        if len(picture_list) == 0:
+            self.bot.say("Could not retrieve a picture from the results.")
+            return None
+        else:
+            return random.choice(picture_list)
 
 def setup(bot):
     bot.add_cog(RedditPics(bot))
